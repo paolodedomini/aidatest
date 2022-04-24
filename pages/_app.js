3d0c97ece0ef4562090851abfaa127ac7aac4b6b
grafica <paolo@monema.it>
2022-04-24 23:13:33 +0200

    test loading api

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,39 @@
+import Router from "next/router";
+import React from "react";
 import '../styles/globals.scss'
 
 import Layout from '../layout/layout'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+  return (
+  
+    <>
+    {loading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <Component {...pageProps} />
+    )}
+  </>
+  
+  )
 
 }
 
