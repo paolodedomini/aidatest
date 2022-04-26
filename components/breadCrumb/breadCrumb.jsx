39810ac49f114501039b030ad5d98e90d6fd5a70
grafica <paolo@monema.it>
2022-04-26 23:58:45 +0200

    dati dei post

--- components/breadCrumb/breadCrumb.jsx
+++ components/breadCrumb/breadCrumb.jsx
@@ -1,13 +1,13 @@
 import style from './breadcrumb.module.scss';
 import Link from 'next/link'
+import { unslugify } from "unslugify";
 
 function BreadCrumb({path, router}){
     console.log(router);
     const linkPath = router.asPath.split('/')
     linkPath.shift();
-
     const pathArray = linkPath.map((path, i) => {
-        return { breadcrumb: path, href: '/' + linkPath.slice(0, i + 1).join('/') };
+        return { breadcrumb: unslugify(path), href: '/' + linkPath.slice(0, i + 1).join('/') };
     });
     console.log(pathArray);
     return (
--- pages/magazine/[slug].jsx
+++ pages/magazine/[slug].jsx
@@ -1,35 +1,78 @@
-import {menuData, magazineData } from '../../lib/queries'
+import { menuData, magazineData, singoloPost } from '../../lib/queries'
 import { useRouter } from 'next/router'
 import Layout from '../../layout/layout'
-const MagazinePage = ({magazine, menu}) => {
-    
-    const router = useRouter()
-    const { slug } = router.query
-    const dataSingoloArticolo = magazine.data.find((item)=> item.slug === slug)
-console.log('router', router);
-    return (
-    
+import { BASE_URL_IMMAGINI } from '../../lib/costanti'
+import Image from 'next/image'
+import style from './magazine.module.scss'
+
+const MagazinePage = ({menu, singlePost }) => {
+  const router = useRouter()
+  const { slug } = router.query
+  const dataPubblicazione = new Date(singlePost.startDate).toLocaleString('en', { day: 'numeric', month: 'long', year: 'numeric' })
+  console.log('articolo', singlePost);
+  return (
+
     <Layout menu={menu} router={router}>
-        <p>{dataSingoloArticolo.title}</p>
+      <section className="magazine-page">
+        <div className="col-1">
+          <div className={style.articoloImage}>
+            {singlePost.thumbnail ? <Image
+              src={`${BASE_URL_IMMAGINI}${singlePost.thumbnail?.images[0].fullPath}`}
+              width={450}
+              height={250}
+              blurDataURL='/images/back_main_menu.png'
+              placeholder="blur"
+              layout='intrinsic'
+            /> : <Image
+              src="/images/placeholder.jpg"
+              width={450}
+              height={250}
+              quality={100}
+              blurDataURL='/images/back_main_menu.png'
+              placeholder="blur"
+              layout='intrinsic'
+            />}
+          </div>
+          <div className={style.specs}>
+            <div className={style.category}>
+              <sub>Category</sub>
+             {singlePost.category.name}
+            </div>
+          </div>
+        </div>
+        <div className="col-2">
+          <div className={style.titoli}>
+            <h2>
+              <sub className={style.data}>{dataPubblicazione}</sub>
+              {singlePost.name}
+            </h2>
+          </div>
+          <div className={style.corpoTesto}>
+            <p>
+              {singlePost.abstract}
+            </p>
+          </div>
+        </div>
+      </section>
     </Layout>
-    )
-  }
-  
+  )
+}
 
-  export async function getServerSideProps(context) {
 
-    const [menuRes, magRes] = await Promise.all([
-      fetch(menuData('it')),
-      fetch(magazineData('it'))
-  
-    ]);
-    const [ menu, magazine] = await Promise.all([
-      menuRes.json(),
-      magRes.json()
-    ]);
-  
-    return { props: { menu, magazine } };
-  
-  
-  }
-  export default MagazinePage
+export async function getServerSideProps(context) {
+const {slug} = context.query
+  const [menuRes, postRes] = await Promise.all([
+    fetch(menuData('it')),
+    fetch(singoloPost('en', 'workshop-7th-aida-international-meeting-day-4-uveitis-and-scleritis'))
+
+  ]);
+  const [menu,  singlePost] = await Promise.all([
+    menuRes.json(),
+    postRes.json()
+  ]);
+const x = context.query
+  return { props: { menu, singlePost } };
+
+
+}
+export default MagazinePage
